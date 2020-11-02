5e3897ad16f963efc87ef19a55b83948334f6e6e
shawn <shawnzhao233@gmail.com>
2020-11-03 00:32:36 +0800

    alpha ver0.0002

---
+++ Assets/Scripts/openTradeStation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openTradeStation : MonoBehaviour
+{
+    bool stationStatus = false;
+    public void ToggleTrade()
+    {
+        if (stationStatus) {
+            this.transform.localPosition = new Vector3(1500, -650, 0);
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(0, -650, 0);
+        }
+        stationStatus = !stationStatus;
+    }
+}
